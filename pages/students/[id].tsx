71e28eb8a36b709457475944189cb6decee66900
Anxhid-Mohed <mohammedanshid100@gmail.com>
2023-01-19 22:30:19 +0530

    updations

--- pages/students/[id].tsx
+++ pages/students/[id].tsx
@@ -1,3 +1,7 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import Style from '../../styles/student.module.css'
+
 export const getStaticPaths = async() =>{
     const res = await fetch ('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
@@ -25,6 +29,7 @@ export const getStaticProps = async(context:any) => {
 }
 
 const Details = ({user}:any) => {
+    const router = useRouter()
     return ( 
         <div>
             <h2>{user.name}</h2>
@@ -32,6 +37,11 @@ const Details = ({user}:any) => {
             <p>{user.website}</p>
             <p>{user.address.city}</p>
             <p>{user.phone}</p>
+            <div className={Style.buttons}>
+                <Link legacyBehavior href={'/students'}><button className={Style.btn}>Back</button></Link> 
+                <Link legacyBehavior href={'/'}><button className={Style.btn1}>Home</button></Link>
+            </div>
+
         </div>
     );
 }
